261e47b54a696bcb7d922a29cc48f6c3bfdef11e
Cedric Matalog <cedricmatalog@gmail.com>
2025-04-21 18:37:02 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    files: ["**/__tests__/**/*.{js,jsx,ts,tsx}"],
-    rules: {
-      "react/display-name": "off"
-    }
-  }
 ];
 
 export default eslintConfig;
